7349c49c9bd55a8a27569686d792cf085edb431d
Nurul Amin <NurulAmin7990@gmail.com>
2020-07-24 20:02:40 +0100

    Update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
@@ -11,6 +12,18 @@ class ProductsController extends Controller
     }
 
     public function addProducts() {
+        $data = request()->validate([
+            'category_name' => 'required',
+            'image' => 'required|file|image'
+        ]);
+
+        $file = request()->category_image->getClientOriginalName();
+        request()->category_image->storeAs('images/products', $file, 'public');
+
+        $category = new Product();
+        $category->name = request('category_name');
+        $category->image = $file;
+        $category->save();
         return view('products.add-products');
     }
 }

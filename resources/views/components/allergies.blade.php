6babe317d1ab91abf552ce57ef0a947a3a6f15b2
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2020-07-24 19:29:18 +0100

    swapped label and input

--- resources/views/components/allergies.blade.php
+++ resources/views/components/allergies.blade.php
@@ -1,56 +1,56 @@
 <div id="allergies">
     <div class="allergy">
-        <label for="corn">Corn</label>
         <input type="checkbox" value="Corn" id="corn">
+        <label for="corn">Corn</label>
     </div>
 
     <div class="allergy">
-        <label for="eggs">Eggs</label>
         <input type="checkbox" value="Eggs" id="eggs">
+        <label for="eggs">Eggs</label>
     </div>
 
     <div class="allergy">
-        <label for="fish">Fish</label>
         <input type="checkbox" value="Fish" id="fish">
+        <label for="fish">Fish</label>
     </div>
 
     <div class="allergy">
-        <label for="meat">Meat</label>
         <input type="checkbox" value="Meat" id="meat">
+        <label for="meat">Meat</label>
     </div>
 
     <div class="allergy">
-        <label for="milk">Milk</label>
         <input type="checkbox" value="Milk" id="milk">
+        <label for="milk">Milk</label>
     </div>
 
     <div class="allergy">
-        <label for="peanut">Peanut</label>
         <input type="checkbox" value="Peanut" id="peanut">
+        <label for="peanut">Peanut</label>
     </div>
 
     <div class="allergy">
-        <label for="shellfish">Shellfish</label>
         <input type="checkbox" value="Shellfish" id="shellfish">
+        <label for="shellfish">Shellfish</label>
     </div>
 
     <div class="allergy">
-        <label for="soy">Soy</label>
         <input type="checkbox" value="Soy" id="soy">
+        <label for="soy">Soy</label>
     </div>
 
     <div class="allergy">
-        <label for="free_nut">Free nut</label>
         <input type="checkbox" value="Free nut" id="free_nut">
+        <label for="free_nut">Free nut</label>
     </div>
 
     <div class="allergy">
-        <label for="wheat">Wheat</label>
         <input type="checkbox" value="Wheat" id="wheat">
+        <label for="wheat">Wheat</label>
     </div>
 
     <div class="allergy">
-        <label for="fpies">FPIES</label>
         <input type="checkbox" value="FPIES" id="fpies">
+        <label for="fpies">FPIES</label>
     </div>
 </div>

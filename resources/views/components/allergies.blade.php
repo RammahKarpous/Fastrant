f4259806e04bb4f211e18e3598e2156efed894fa
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2020-07-26 19:26:16 +0100

    Added the name attribute to the allergies

--- resources/views/components/allergies.blade.php
+++ resources/views/components/allergies.blade.php
@@ -1,56 +1,56 @@
 <div id="allergies">
     <div class="allergy checkbox">
-        <input type="checkbox" value="Corn" id="corn">
+        <input type="checkbox" name="allergies[]" value="Corn" id="corn">
         <label for="corn">Corn</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Eggs" id="eggs">
+        <input type="checkbox" name="allergies[]" value="Eggs" id="eggs">
         <label for="eggs">Eggs</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Fish" id="fish">
+        <input type="checkbox" name="allergies[]" value="Fish" id="fish">
         <label for="fish">Fish</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Meat" id="meat">
+        <input type="checkbox" name="allergies[]" value="Meat" id="meat">
         <label for="meat">Meat</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Milk" id="milk">
+        <input type="checkbox" name="allergies[]" value="Milk" id="milk">
         <label for="milk">Milk</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Peanut" id="peanut">
+        <input type="checkbox" name="allergies[]" value="Peanut" id="peanut">
         <label for="peanut">Peanut</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Shellfish" id="shellfish">
+        <input type="checkbox" name="allergies[]" value="Shellfish" id="shellfish">
         <label for="shellfish">Shellfish</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Soy" id="soy">
+        <input type="checkbox" name="allergies[]" value="Soy" id="soy">
         <label for="soy">Soy</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Free nut" id="free_nut">
+        <input type="checkbox" name="allergies[]" value="Free nut" id="free_nut">
         <label for="free_nut">Free nut</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="Wheat" id="wheat">
+        <input type="checkbox" name="allergies[]" value="Wheat" id="wheat">
         <label for="wheat">Wheat</label>
     </div>
 
     <div class="allergy checkbox">
-        <input type="checkbox" value="No Allergies" id="no_allergies">
+        <input type="checkbox" name="allergies[]" value="No Allergies" id="no_allergies">
         <label for="no_allergies">No allergies</label>
     </div>
 

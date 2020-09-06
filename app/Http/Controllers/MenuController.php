b3cdd1445d4a3a5f5d04baeb993009213f824fa8
riazzzy <47174848+riazzzy@users.noreply.github.com>
2020-09-06 19:21:14 +0100

    Update MenuController.php
    sonmething about facades

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Menu;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class MenuController extends Controller
 {
